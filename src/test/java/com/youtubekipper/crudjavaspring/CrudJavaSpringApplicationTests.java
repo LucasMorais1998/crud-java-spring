fc458da5ae50754c6a06e1755950e8239a144157
Lucas Morais <lucas14.morais@gmail.com>
2023-12-06 21:55:21 -0300

    initial commit

---
+++ src/test/java/com/youtubekipper/crudjavaspring/CrudJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youtubekipper.crudjavaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
