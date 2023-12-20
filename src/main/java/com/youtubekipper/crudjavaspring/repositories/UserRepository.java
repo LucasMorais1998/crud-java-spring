7df742e749070ee70d34edfe1ca583daf204159a
Lucas Morais <lucas14.morais@gmail.com>
2023-12-19 23:40:03 -0300

    feat(user): add UserRepository

---
+++ src/main/java/com/youtubekipper/crudjavaspring/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.youtubekipper.crudjavaspring.repositories;
+
+import com.youtubekipper.crudjavaspring.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    UserDetails findByLogin(String login);
+}
