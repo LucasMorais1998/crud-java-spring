10421705a3b82c9bf3b7a75ffc4a616a8a8e856a
Lucas Morais <lucas14.morais@gmail.com>
2023-12-22 16:38:21 -0300

    feat(auth): add AuthorizationService

---
+++ src/main/java/com/lucas/crudjavaspring/services/AuthorizationService.java
@@ -0,0 +1,21 @@
+package com.lucas.crudjavaspring.services;
+
+import com.lucas.crudjavaspring.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorizationService implements UserDetailsService {
+    UserRepository userRepository;
+
+    public AuthorizationService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByLogin(username);
+    }
+}
